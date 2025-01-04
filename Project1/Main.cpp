72aa22d5fdbf5576d768233fcc44c64cfed3fa1f
luntear <mszopa20@gmail.com>
2025-01-04 17:15:42 +0100

    Add project files.

---
+++ Project1/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+int dzielenie(int x, int y, int c);
+
+int main()
+{
+    
+    int x = 6;
+    int y = 2;
+    int c=0;
+
+    c=dzielenie(x,y,c);
+
+
+
+    std::cout << "Hello World!\n"<< c;
+    return 0;
+
+}
+int dzielenie(int x, int y, int c)
+{
+
+    c = x / y;
+    return c;
+
+}
