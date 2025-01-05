abffa25afb4bafbb37624e00cb5dc14128197b91
luntear <mszopa20@gmail.com>
2025-01-05 04:10:50 +0100

    uga buga

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -7,12 +7,17 @@ int main()
     int x = 6;
     int y = 2;
     int c=0;
-
+    std::cin >> x; "\n";
+    std::cout << "asdasd" << std::endl;
+    
+    std::cin >> y;
     c=dzielenie(x,y,c);
 
+    std::cout << "to x " << x << "to y" << y;
 
+    std::cout << "Hello World!"<< c<<std::endl;
+    std::cout << "test" << std::endl;
 
-    std::cout << "Hello World!\n"<< c;
     return 0;
 
 }
